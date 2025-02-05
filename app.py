b87adb10ac22a31197d4605346278a97ef4c88fb
dededc37 <134894425+dededc37@users.noreply.github.com>
2025-02-05 15:02:37 -0300

    acrescentando incerção de código

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from dados.usuarios import *
 
 app = Flask(__name__)
+codigo = 000000
 
 @app.route('/login')
 def login():
@@ -10,11 +11,25 @@ def login():
 
 @app.route('/login/code')
 def login_code():
+    global codigo
     codigo = int(request.args.get('codigo'))
-    if usuarios[codigo]["nome"] == '':
-        return('batata')
+    if codigo in usuarios:
+        if usuarios[codigo]["nome"] == '':
+            return(render_template('login_nome.html'))
+        else:
+            return('nome cadastrado')
     else:
-        return('torresmo')
+        return(redirect('/login'))
+    
+@app.route('/login/nome')
+def login_nome():
+    return(render_template('login_nome.html'))
+
+@app.route('/login/cad_nome')
+def cad_nome():
+    nome = str(request.args.get('nome'))
+    usuarios[codigo]["nome"] = nome
+    return('nome cadastrado')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
--- dados/usuarios.py
+++ dados/usuarios.py
@@ -1,5 +1,5 @@
 usuarios = {
-    123456: {"nome": "André Huber", "is_admin": True},
+    123456: {"nome": "", "is_admin": True},
     678906: {"nome": "João Silva", "is_admin": False},
     112236: {"nome": "Maria Oliveira", "is_admin": False}
 }
