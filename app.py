97aa440d177343a002afeda25330c189866ea26d
dededc37 <134894425+dededc37@users.noreply.github.com>
2025-02-05 14:27:50 -0300

    requerimento do código por get

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import *
 import os
+from dados.usuarios import *
 
 app = Flask(__name__)
 
 @app.route('/login')
 def login():
     return(render_template('login.html'))
+
+@app.route('/login/code')
+def login_code():
+    codigo = int(request.args.get('codigo'))
+    if usuarios[codigo]["nome"] == '':
+        return('batata')
+    else:
+        return('torresmo')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
---
+++ dados/usuarios.py
@@ -0,0 +1,5 @@
+usuarios = {
+    123456: {"nome": "André Huber", "is_admin": True},
+    678906: {"nome": "João Silva", "is_admin": False},
+    112236: {"nome": "Maria Oliveira", "is_admin": False}
+}
